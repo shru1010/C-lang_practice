e4154290bc6c9c0d2f36ae607e83f398cfaae384
shru1010 <66427446+shru1010@users.noreply.github.com>
2021-11-09 16:47:19 +0530

    Add files via upload
     Write a C program to check two given integers, and return true if one of them is 30 or if their sum is 30.

---
+++ clang-3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+
+	int a,b;
+	bool x=true;
+	printf("what is a");
+	scanf("%d",&a);
+	printf("what is b");
+        scanf("%d",&b);
+	if(a==30||b==30||a+b==30)
+		printf("%d",x);
+		return x;
+
+}
